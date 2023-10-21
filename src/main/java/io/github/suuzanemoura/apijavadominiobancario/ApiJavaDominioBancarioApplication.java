dde5ef3f1f8d17c6dd5b22bf7edbcc69cab6cecf
Suzane Moura <suuzanemoura@gmail.com>
2023-10-21 12:58:33 -0300

    first commit

---
+++ src/main/java/io/github/suuzanemoura/apijavadominiobancario/ApiJavaDominioBancarioApplication.java
@@ -0,0 +1,13 @@
+package io.github.suuzanemoura.apijavadominiobancario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJavaDominioBancarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiJavaDominioBancarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/suuzanemoura/apijavadominiobancario/ApiJavaDominioBancarioApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.suuzanemoura.apijavadominiobancario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiJavaDominioBancarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
